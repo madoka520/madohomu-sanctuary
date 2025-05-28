a0bfb20ce6de60bfac5f6e28557ac19c4d201770
madoka <madokalove@qq.com>
2025-05-29 00:29:39 +0800

    悬浮特效, 网站图标

--- src/components/PictureAlbumCard.vue
+++ src/components/PictureAlbumCard.vue
@@ -1,7 +1,9 @@
 <!--画册组件-->
 <template>
-  <div class="frame" :class="Root.class" :style="Root.style">
-    {{ msg }}
+  <div class="frame__wrapper" :style="Root.wrapperStyle" :class="Root.class" >
+    <div class="frame" >
+      {{ msg }}
+    </div>
   </div>
 </template>
 <script setup lang="ts">
@@ -24,10 +26,12 @@ const Root = (() => {
   const buildLength = () => Math.floor(Math.random() * (200 - 180 + 1)) + 180;
   const s = reactive({
     class: computed(() => ({
+      //随机取一个frame类
       [props.className ?? `frame${Math.floor(Math.random() * 2)}`]: true,
+      //随机移动方式
       [moveClassList[`${Math.floor(Math.random() * 3)}`]]: true
     })),
-    style: computed<CSSProperties>(() => {
+    wrapperStyle: computed<CSSProperties>(() => {
       const maxWidth = buildLength();
       const maxHeight = buildLength();
 
@@ -44,22 +48,41 @@ const Root = (() => {
         position: "absolute",
         left: `${left}px`,
         top: `${top}px`,
-        maxWidth: `${maxWidth}px`,
-        maxHeight: `${maxHeight}px`,
+        width: `${maxWidth}px`,
+        height: `${maxHeight}px`,
       };
     }),
   });
   return s;
 })();
 </script>
-<style scoped>
-.frame {
-  border: 10px solid #837e7a;
-  min-width: 200px;
-  min-height: 200px;
-  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.3);
-  overflow: auto;
+<style scoped lang="less">
+.frame__wrapper {
+  max-width: 200px;
+  max-height: 200px;
+  &:hover {
+    z-index: 10;
+  }
+
+  .frame {
+    border: 10px solid #837e7a;
+    width: 100%;
+    height: 100%;
+    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.3);
+    overflow: auto;
+    transition: transform 0.3s ease, filter 0.3s ease, box-shadow 0.3s ease;
+  }
+  .frame:hover {
+    transform: scale(1.1); // 放大到 105%
+    filter: brightness(1.3); // 稍微提亮
+    box-shadow:
+      0 12px 24px rgba(0, 0, 0, 0.3),
+      0 0 12px rgba(255, 200, 255, 0.4); // 柔光边框
+  }
 }
+
+
+
 
 .frame0 {
   border-radius: 10px;
