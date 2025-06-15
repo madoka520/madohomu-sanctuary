06840a6348da2439e0162ec2fac709ea8f9abe5a
madoka <madokalove@qq.com>
2025-06-16 01:21:03 +0800

    遮罩组件 弹窗(未完成)

---
+++ src/components/MadokaDialog.vue
@@ -0,0 +1,25 @@
+<template>
+  <madoka-mask v-model="modelValue" @close="Root.cancel">
+
+  </madoka-mask>
+
+</template>
+<script setup lang="ts">
+import MadokaMask from "@/components/MadokaMask.vue";
+
+const modelValue = defineModel({
+  default: false
+})
+const Root = (() => {
+  const cancel = (e: Event) => {
+
+  };
+  const s = reactive({
+    cancel
+  })
+  return s
+})()
+</script>
+<style scoped>
+
+</style>
---
+++ src/components/MadokaMask.vue
@@ -0,0 +1,36 @@
+<template>
+  <teleport to="body">
+    <div class="mask" v-show="modelValue" @click="Root.close">
+    </div>
+  </teleport>
+
+</template>
+<script setup lang="ts">
+
+const modelValue = defineModel({
+  default: false
+})
+const emits = defineEmits<{
+  (e: "close", event: Event): void
+}>()
+const Root = (() => {
+  const close = (e:Event) => {
+    modelValue.value = false
+    emits('close', e)
+  };
+  const s = reactive({
+    close
+  })
+  return s
+})()
+</script>
+<style scoped>
+.mask {
+  position: fixed;
+  inset: 0;
+  width: 100vw;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.5);
+  z-index: 999999;
+}
+</style>
