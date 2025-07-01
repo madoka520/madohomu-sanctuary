7da51616511d063760ec46f966630c300f9058f4
圆环之理 <madokalove@qq.com>
2025-07-01 17:07:03 +0800

    弹窗

--- src/components/MadokaDialog.vue
+++ src/components/MadokaDialog.vue
@@ -1,12 +1,28 @@
 <template>
   <madoka-mask v-model="modelValue" @close="Root.cancel">
+    <div class="dialog__container">
 
+    </div>
+    <slot/>
+    <slot name="footer">
+      <button>确定</button>
+      <button>取消</button>
+    </slot>
   </madoka-mask>
 
 </template>
 <script setup lang="ts">
 import MadokaMask from "@/components/MadokaMask.vue";
-
+const props = withDefaults(
+  defineProps<{
+    width?: string
+    height?: string
+    footer?: boolean
+  }>(),
+  {
+    footer: true
+  }
+)
 const modelValue = defineModel({
   default: false
 })
@@ -21,5 +37,8 @@ const Root = (() => {
 })()
 </script>
 <style scoped>
-
+.dialog__container {
+  width: v-bind(width);
+  height: v-bind(height);
+}
 </style>
