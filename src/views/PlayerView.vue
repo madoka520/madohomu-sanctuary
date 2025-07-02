41c1aa4915a1bc1bdd97358a048158e3017c194e
圆环之理 <madokalove@qq.com>
2025-07-02 18:15:56 +0800

    播放器

--- src/views/PlayerView.vue
+++ src/views/PlayerView.vue
@@ -2,7 +2,7 @@
   <audio :src="music" autoplay loop ref="musicRef"></audio>
 </template>
 <script setup lang="ts">
-import music from "@/assets/musics/君の銀の庭.mp3";
+import music from "@/assets/music/君の銀の庭.mp3";
 const musicRef = useTemplateRef("musicRef");
 const play = () => {
   window.removeEventListener("click", play)
