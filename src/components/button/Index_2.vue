d27c60ef086facdd42d1c99e6222f2eec2a43082
悠米° <yuumigift@qq.com>
2026-01-09 09:57:26 +0800

    按钮粒子

--- src/components/button/Index_2.vue
+++ src/components/button/Index_2.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="['submit-btn', { loading: loading }]">
+  <div :class="['submit-btn', { loading: loading }]" ref="ref_button">
     <span v-show="!loading">
       <slot>
         {{ text }}
@@ -9,6 +9,12 @@
   </div>
 </template>
 <script setup lang="ts">
+import { generateRact } from "@/utils/ParticleUtils.ts"
+import particleStarUrl from "@/assets/particle/star.png"
+import particleStarPlainUrl from "@/assets/particle/star_plain.png"
+import particleHeartUrl from "@/assets/particle/heart.png"
+import particleHeartPlainUrl from "@/assets/particle/heart_plain.png"
+import { animate } from "animejs"
 const props = withDefaults(
   defineProps<{
     text?: string
@@ -18,6 +24,77 @@ const props = withDefaults(
 const loading = defineModel("loading", {
   default: false,
 })
+
+const ref_button = useTemplateRef("ref_button")
+
+const Particle = (() => {
+  const createParticle = (x: number, y: number, w: number, h: number, count: number) => {
+    for (let i = 0; i < count; i++) {
+      const imageMap = {
+        0: particleStar,
+        1: particleHeart,
+        2: particleHeartPlain,
+        3: particleStarPlain,
+      }
+      const image = imageMap[Math.floor(Math.random() * 4)]
+      generateRact(image, x, y, w, h, 0.4, 20)
+    }
+  }
+  const createMouseMoveAnimationInLoginButton = () => {
+    const renderParticle = () => {
+      const rect = ref_button.value!.getBoundingClientRect()
+      createParticle(rect.x - 15, rect.y - 15, rect.width, rect.height, 6)
+    }
+    const onMouseMove = (e: MouseEvent) => {
+      if (!ref_button.value) return
+      const rect = ref_button.value!.getBoundingClientRect()
+      // 判断鼠标是否在按钮上
+      if (e.clientX >= rect.x && e.clientX <= rect.x + rect.width && e.clientY >= rect.y && e.clientY <= rect.y + rect.height) {
+        s.is_hover = true
+      } else {
+        s.is_hover = false
+      }
+    }
+    const enterFrame = () => {
+      if (s.is_destroy) return
+      if (s.is_hover) {
+        if (frame < 1e6) {
+          frame++
+        } else {
+          frame = 0
+        }
+        if (frame % 300 === 0) {
+          renderParticle()
+        }
+      } else {
+        frame = -1
+      }
+      requestAnimationFrame(() => enterFrame())
+    }
+    window.addEventListener("mousemove", onMouseMove)
+    onMounted(() => {
+      enterFrame()
+    })
+    onUnmounted(() => {
+      s.is_destroy = true
+      window.removeEventListener("mousemove", onMouseMove)
+    })
+  }
+  let frame = 0
+  const particleStar = new Image()
+  const particleStarPlain = new Image()
+  const particleHeart = new Image()
+  const particleHeartPlain = new Image()
+  particleStar.src = particleStarUrl
+  particleStarPlain.src = particleStarPlainUrl
+  particleHeart.src = particleHeartUrl
+  particleHeartPlain.src = particleHeartPlainUrl
+  createMouseMoveAnimationInLoginButton()
+  const s = reactive({
+    is_hover: false,
+    is_destroy: false,
+  })
+})()
 </script>
 <style scoped>
 /* 提交按钮 */
--- src/utils/ParticleUtils.ts
+++ src/utils/ParticleUtils.ts
@@ -5,11 +5,12 @@ const move = () => {
     particle.x += particle.vx
     particle.y += particle.vy
     particle.vy += particle.gravity
-    particle.opacity += particle.opacitySpeed
+    particle.opacity += particle.is_growing ? 0.004 : particle.opacitySpeed
     particle.rotate += particle.rotateSpeed
     particle.rotate = particle.rotate % 360
     particle.x = particle.x % window.innerWidth
 
+    if (particle.opacity > particle.opacityLine) particle.is_growing = false
     if (particle.x < -1e5) particle.is_deleted = true
     if (particle.y < -1e5) particle.is_deleted = true
     if (particle.x > window.innerWidth + 1e5) particle.is_deleted = true
@@ -30,10 +31,10 @@ export const generateRact = (img: HTMLImageElement, x: number, y: number, w: num
   const py = y + Math.random() * h
   const vx = speed * Math.cos(Math.random() * Math.PI * 2)
   const vy = speed * Math.sin(Math.random() * Math.PI * 2)
-  const opacity = Math.random() * 0.5 + 0.5
-  const opacitySpeed = -Math.random() * 0.005 - 0.0025
+  const opacityLine = Math.random() * 0.5 + 0.5
+  const opacitySpeed = -Math.random() * 0.006 - 0.002
   const rotate = Math.random() * 360
-  const rotateSpeed = Math.random() * 0.2 - 0.1
+  const rotateSpeed = Math.random() * 0.1 - 0.05
   particle_list.push({
     id: generateId(),
     img,
@@ -43,12 +44,14 @@ export const generateRact = (img: HTMLImageElement, x: number, y: number, w: num
     vy,
     w: size,
     h: size,
-    gravity: 0.003,
-    opacity,
+    gravity: 0.0005,
+    opacity: 0,
+    opacityLine,
     opacitySpeed,
     rotate,
     rotateSpeed,
     is_deleted: false,
+    is_growing: true,
   })
 }
 
@@ -70,5 +73,6 @@ export namespace I {
     opacity: number
     opacitySpeed: number
     is_deleted: boolean
+    [key: string]: any
   }
 }
