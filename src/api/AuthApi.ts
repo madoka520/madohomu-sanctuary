57932ab422c1ec02d04dc5b788907ef6e185ba53
圆环之理 <madokalove@qq.com>
2026-01-21 17:29:38 +0800

    -

--- src/api/AuthApi.ts
+++ src/api/AuthApi.ts
@@ -1,20 +1,20 @@
 import { madokaPost, madokaPut } from "@/utils/request.ts"
-import type { SignedPath, UploadCallback } from "@/types/upload.ts"
-import DriverManager from "@/utils/oss/DriverManager.ts"
-import axios from "axios"
 
 export default {
   login: (data) => madokaPost("/login", data, { showErrorMessage: false }),
   // upload: (data) => madokaPost("/avatar", data),
   upload: async (data, file: File) => {
     // 重新请求 token
-    const res = await madokaPost("/avatar", data)
-    // const res = await axios.post("/oss-api", data)
-    const d = res.data as SignedPath
+    const formData = new FormData();
+    formData.append("file", file);
+    const res = await madokaPost("/avatar", formData)
+    /*    // const res = await axios.post("/oss-api", data)
+    const d = res as unknown as SignedPath
+    console.log(d)
     if (!d) return
 
     const backData: UploadCallback = await DriverManager.getDriver(d.driver).upload(file, d)
-    return backData
+    return backData*/
   },
   updateUsername: (username) => madokaPut("/username", { username }),
   updatePassword: (password) => madokaPut("/password", { password }),
