88c6ee508a4ff8272740543bc2b909ea913da946
madoka <madokalove@qq.com>
2026-02-05 22:09:38 +0800

    -

--- src/components/MadokaMsgCard.vue
+++ src/components/MadokaMsgCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="card__wrapper">
-    <div class="card" :style="cardStyle" :data-time="createTime">
-      <div class="card__header">
+    <div class="flex card" :style="cardStyle" :data-time="createTime">
+      <div class="flex card__header">
         <div class="avatar">
           <img draggable="false" :src="avatarSrc" loading="lazy" alt="" />
         </div>
@@ -13,22 +13,33 @@
 
       <div class="card__content">
         {{ content }}
+        <div v-if="image">
+          <madoka-img :src="`https://haojiezhe12345.top:82/madohomu/api/data/images/posts/${image}.jpg`" />
+        </div>
       </div>
 
-      <div v-if="image">
-        <madoka-img :src="`https://haojiezhe12345.top:82/madohomu/api/data/images/posts/${image}.jpg`" />
-      </div>
       <footer>
-        <div class="card__bar"><i class="mdi mdi-heart" :class="{ liked: +liked }" @click="Root.like"/> {{ likes }}</div>
-        <div class="card__footer">
+        <div class="card__bar">
+          <span>
+            <i class="mdi mdi-heart liked" v-if="+liked" @click="Root.like" />
+            <i class="mdi mdi-heart-outline" v-else @click="Root.like" />
+            <span class="number" v-show="likes !== 0">
+              {{ likes }}
+            </span>
+          </span>
+          <span>
+            <i class="mdi mdi-reply"></i>
+          </span>
+        </div>
+        <div class="flex card__operate">
           <span v-if="origin !== 'madokami'">
-            <a v-if="origin === 'kami'" href="https://kami.im" target="_blank"> kami.im </a>
+            <a v-if="origin === 'kami'" href="https://kami.im" target="_blank">kami.im</a>
             <a v-else :href="`https://${origin}`" target="_blank">
               {{ origin }}
             </a>
           </span>
-          <div style="display: flex; margin-left: auto">
-            {{ dayjs(createTime).format("YYYY/MM/DD HH:mm:ss") }}
+          <div class="flex" style="margin-left: auto">
+            {{ dayjs(createTime).format('YYYY/MM/DD HH:mm:ss') }}
           </div>
         </div>
       </footer>
@@ -37,12 +48,12 @@
 </template>
 
 <script setup lang="ts">
-import dayjs from "dayjs"
-import { getAssetUrl, getAvatarUrl, getImgUrl } from "@/utils/resource.ts"
-import { computed } from "vue"
-import axios from "axios"
-import MadokaImg from "@/components/MadokaImg.vue"
-import messageApi from "@/api/MessageApi.ts"
+import dayjs from 'dayjs'
+import { getAssetUrl, getAvatarUrl, getImgUrl } from '@/utils/resource.ts'
+import { computed } from 'vue'
+import axios from 'axios'
+import MadokaImg from '@/components/MadokaImg.vue'
+import messageApi from '@/api/MessageApi.ts'
 
 const props = withDefaults(
   defineProps<{
@@ -53,7 +64,7 @@ const props = withDefaults(
     username: string
     externalId: number
     updateTime: number
-    origin?: "madokami" | "kami" | string
+    origin?: 'madokami' | 'kami' | string
     avatar?: string
     image?: string
     likes: number
@@ -61,13 +72,13 @@ const props = withDefaults(
   }>(),
   {},
 )
-const emits = defineEmits(["like"])
+const emits = defineEmits(['like'])
 const avatarSrc = computedAsync(async () => {
-  if (props.origin === "madohomu.love") {
-    const res = await axios.get("https://haojiezhe12345.top:82/madohomu/api/user/find", { params: { id: props.uid } })
+  if (props.origin === 'madohomu.love') {
+    const res = await axios.get('https://haojiezhe12345.top:82/madohomu/api/user/find', { params: { id: props.uid } })
     return `https://haojiezhe12345.top:82/madohomu/api/data/images/avatars/${res.data[0]?.avatar ?? -1}`
   }
-  return props.origin === "madokami" ? `${getAvatarUrl(props.uid)}?t=${props.updateTime}` : `https://kami.im/getavatar.php?uid=${props.uid}`
+  return props.origin === 'madokami' ? `${getAvatarUrl(props.uid)}?t=${props.updateTime}` : `https://kami.im/getavatar.php?uid=${props.uid}`
 })
 
 const cardStyle = computed(() => {
@@ -75,17 +86,20 @@ const cardStyle = computed(() => {
   const fixedNum = (props.externalId % 22) + 1
   const bgUrl = getAssetUrl(`madokami/msg_bg/bg_${fixedNum}`)
   return {
-    "--bg-image": `url(${bgUrl}.webp)`,
+    '--bg-image': `url(${bgUrl}.webp)`,
   }
 })
 
 const Root = (() => {
+  /**
+   * 发送请求 改变该条留言的喜爱状态
+   */
   const like = async () => {
     await messageApi.like(props.messageId)
     emits('like')
   }
   const s = reactive({
-    like
+    like,
   })
   return s
 })()
@@ -118,20 +132,16 @@ const Root = (() => {
 
     background: rgba(255, 255, 255, 0.35);
     backdrop-filter: blur(8px);
-    border-radius: 14px;
-
+    border-radius: 26px;
+    corner-shape: superellipse(1.333);
     box-shadow: 0 8px 24px rgba(0, 0, 0, 0.12);
     color: white;
 
-    display: flex;
     flex-direction: column;
     gap: 12px;
-    --smooth-level: 32;
-    -webkit-mask-image: paint(smooth-corners);
-    mask-image: paint(smooth-corners);
     // 使用伪元素处理背景，这样 blur 滤镜才不会影响到文字
     &::before {
-      content: "";
+      content: '';
       position: absolute;
       inset: 0;
       z-index: -1;
@@ -151,7 +161,6 @@ const Root = (() => {
   }
 
   .card__header {
-    display: flex;
     align-items: center;
     gap: 10px;
 
@@ -193,15 +202,27 @@ const Root = (() => {
     display: flex;
     flex-direction: column;
   }
+
   .card__bar {
     font-size: 20px;
     margin-left: auto;
+    & > span {
+      display: inline-flex;
+      width: 35px;
+      height: 35px;
+      align-items: center;
+      justify-content: center;
+      border-radius: 8px;
+      &:hover {
+        background: gray;
+      }
+    }
     .liked {
       color: red;
     }
   }
-  .card__footer {
-    display: flex;
+
+  .card__operate {
     font-size: 12px;
     opacity: 0.6;
     width: 100%;
@@ -209,7 +230,7 @@ const Root = (() => {
     a {
       // 基础样式重置
       text-decoration: none;
-      color: white; // 稍微带点神秘的紫色
+      color: white;
       font-weight: 500;
       position: relative;
       transition: color 0.3s ease;
@@ -217,7 +238,7 @@ const Root = (() => {
 
       // 使用伪元素制作下划线动画
       &::after {
-        content: "";
+        content: '';
         position: absolute;
         bottom: 0;
         left: 0;
