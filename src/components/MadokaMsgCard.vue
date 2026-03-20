12dfd22a8c357b7afdeb3bce9c46ac7d255efc8e
madoka <madokalove@qq.com>
2026-03-21 04:15:47 +0800

    兼容移动端

--- src/components/MadokaMsgCard.vue
+++ src/components/MadokaMsgCard.vue
@@ -238,7 +238,7 @@ const Root = (() => {
   }
   .card {
     width: clamp(260px, 22vw, 520px);
-    height: clamp(240px, 55vh, 700px);
+    height: clamp(140px, 55vh, 700px);
     overflow: hidden;
     position: relative; // 必须是 relative 才能让伪元素定位
     isolation: isolate; // 确保内容层级在伪元素之上
