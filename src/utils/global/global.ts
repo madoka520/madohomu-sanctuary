7af62d096dc14b56f14d1924548c812648b4bc21
madoka <madokalove@qq.com>
2026-03-30 23:23:55 +0800

    -

--- src/utils/global/global.ts
+++ src/utils/global/global.ts
@@ -2,4 +2,5 @@ export const html = String.raw
 export const sleep = async (timeout: number) =>
   new Promise((resolve, reject) => setTimeout(resolve, timeout))
 export const isMobile = () =>
-  /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent)
+  /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent) ||
+  !window.matchMedia('(pointer: fine)').matches
