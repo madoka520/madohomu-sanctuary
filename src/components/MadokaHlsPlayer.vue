4325e5a925cf116fcda75d65396363abdfeea75c
madoka <madokalove@qq.com>
2026-07-18 00:01:24 +0800

    chore: untrack package-lock.json

---
+++ src/components/MadokaHlsPlayer.vue
@@ -0,0 +1,90 @@
+<template>
+  <video ref="videoRef" class="madoka-hls-player" controls autoplay></video>
+</template>
+
+<script setup lang="ts">
+import Hls from 'hls.js'
+
+const props = withDefaults(
+  defineProps<{
+    src?: string
+  }>(),
+  {
+    
+  }
+)
+const videoRef = useTemplateRef("videoRef")
+const Player = (() => {
+  let hlsInstance: Hls | null = null
+
+  const f = {
+    play: () => {
+      const video = videoRef.value
+      if (!video) return
+      video.play().catch(() => {})
+    },
+    pause: () => {
+      const video = videoRef.value
+      if (!video) return
+      video.pause()
+    },
+    destroy: () => {
+      if (hlsInstance) {
+        hlsInstance.destroy()
+        hlsInstance = null
+      }
+    }
+  }
+
+  const initHls = (url: string) => {
+    const video = videoRef.value
+    if (!video) return
+
+    f.destroy()
+
+    if (Hls.isSupported()) {
+      hlsInstance = new Hls()
+      hlsInstance.loadSource(url)
+      hlsInstance.attachMedia(video)
+      hlsInstance.on(Hls.Events.MANIFEST_PARSED, () => {
+        f.play()
+      })
+    } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+      video.src = url
+      video.addEventListener('loadedmetadata', () => {
+        f.play()
+      })
+    }
+  }
+
+  const init = () => {}
+
+  const initOnMounted = () => {
+    if (s.src) {
+      initHls(s.src)
+    }
+  }
+
+  const s = reactive({
+    src: '',
+    ...f
+  })
+
+  init()
+  onMounted(initOnMounted)
+
+  onUnmounted(() => {
+    f.destroy()
+  })
+
+  return s
+})()
+</script>
+
+<style scoped>
+.madoka-hls-player {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+}
+</style>
